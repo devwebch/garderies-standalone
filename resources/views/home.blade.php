0a5191a4ec83b67ebc3e1ee7434d70eb97562c9c
Simon Rapin <simon.rapin@gmail.com>
2018-05-09 12:06:30 +0200

    -updated: dashboard column display style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,17 @@
     <div class="card card-default dashboard__summary">
         <div class="card-body">
             <div class="row">
-                <div class="col widget-count">
+                <div class="col-md-4 widget-count">
                     <h3 style="font-size: 1em">Garderies dans votre réseau</h3>
                     <div class="number" style="font-size: 4em;">12</div>
                     <a href="{{route('nurseries.index')}}">Gérer mes garderies</a>
                 </div>
-                <div class="col widget-count">
+                <div class="col-md-4 widget-count">
                     <h3 style="font-size: 1em">Nombre d'employés</h3>
                     <div class="number" style="font-size: 4em;">56</div>
                     <a href="{{route('users.index')}}">Gérer mes employés</a>
                 </div>
-                <div class="col widget-count">
+                <div class="col-md-4 widget-count">
                     <h3 style="font-size: 1em">Remplacements ce mois</h3>
                     <div class="number" style="font-size: 4em;">34</div>
                     <a href="#">Gérer les remplacements</a>
