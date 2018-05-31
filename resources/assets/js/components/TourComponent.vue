834d96e122bcfe21e48e8dc79c68072598102d0b
Simon Rapin <simon.rapin@gmail.com>
2018-05-31 09:00:48 +0200

    -updated: the home tour is now contained to its own component

--- resources/assets/js/components/TourComponent.vue
+++ resources/assets/js/components/TourComponent.vue
@@ -1,3 +1,32 @@
+<template>
+    <v-tour name="myTour" :steps="steps">
+        <template slot-scope="tour">
+            <transition name="fade">
+                <v-step
+                        v-if="tour.currentStep === index"
+                        v-for="(step, index) of tour.steps"
+                        :key="index"
+                        :step="step"
+                        :previous-step="tour.previousStep"
+                        :next-step="tour.nextStep"
+                        :stop="tour.stop"
+                        :is-first="tour.isFirst"
+                        :is-last="tour.isLast"
+                >
+                    <template>
+                        <div slot="actions">
+                            <button @click="tour.stop" v-if="!tour.isLast" class="v-step__button">Passer le tour</button>
+                            <button @click="tour.previousStep" v-if="!tour.isFirst" class="v-step__button">Précédent</button>
+                            <button @click="tour.nextStep" v-if="!tour.isLast" class="v-step__button">Suivant</button>
+                            <button @click="tour.stop" v-if="tour.isLast" class="v-step__button">Terminer</button>
+                        </div>
+                    </template>
+                </v-step>
+            </transition>
+        </template>
+    </v-tour>
+</template>
+
 <script>
     import VueTour from 'vue-tour';
     import 'vue-tour/dist/vue-tour.css';
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,36 +29,7 @@
             </div>
         </div>
     </div>
-    <tour inline-template>
-        <div>
-            <v-tour name="myTour" :steps="steps">
-                <template slot-scope="tour">
-                    <transition name="fade">
-                        <v-step
-                                v-if="tour.currentStep === index"
-                                v-for="(step, index) of tour.steps"
-                                :key="index"
-                                :step="step"
-                                :previous-step="tour.previousStep"
-                                :next-step="tour.nextStep"
-                                :stop="tour.stop"
-                                :is-first="tour.isFirst"
-                                :is-last="tour.isLast"
-                        >
-                            <template>
-                                <div slot="actions">
-                                    <button @click="tour.stop" v-if="!tour.isLast" class="v-step__button">Passer le tour</button>
-                                    <button @click="tour.previousStep" v-if="!tour.isFirst" class="v-step__button">Précédent</button>
-                                    <button @click="tour.nextStep" v-if="!tour.isLast" class="v-step__button">Suivant</button>
-                                    <button @click="tour.stop" v-if="tour.isLast" class="v-step__button">Terminer</button>
-                                </div>
-                            </template>
-                        </v-step>
-                    </transition>
-                </template>
-            </v-tour>
-        </div>
-    </tour>
+    <tour></tour>
 @endsection
 
 @section('scripts')
