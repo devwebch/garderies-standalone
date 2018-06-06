697ebc51a4b4529016dc760d3bb097dd6d5f46d0
Simon Rapin <simon.rapin@gmail.com>
2018-06-06 15:15:41 +0200

    -updated: simplified Availability model

--- app/Http/Resources/Availability.php
+++ app/Http/Resources/Availability.php
@@ -15,13 +15,10 @@ class Availability extends JsonResource
      */
     public function toArray($request)
     {
-        $start  = Carbon::parse($this->start);
-        $end    = Carbon::parse($this->end);
-
-        $start_day  = $start->format('d.m.Y');
-        $end_day    = $end->format('d.m.Y');
-        $start_hour = $start->format('H\hi');
-        $end_hour   = $end->format('H\hi');
+        $start_day  = $this->start->format('d.m.Y');
+        $end_day    = $this->end->format('d.m.Y');
+        $start_hour = $this->start->format('H\hi');
+        $end_hour   = $this->end->format('H\hi');
 
         $data = parent::toArray($request);
         $data['matching']           = $this->matching;
