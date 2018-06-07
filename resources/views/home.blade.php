29f14d46ee807807cb45e1930236373967892e3d
Simon Rapin <simon.rapin@gmail.com>
2018-06-07 14:33:02 +0200

    -updated: home tour -added: welcome alert on the homepage -added: link to start guided tour, uses a randomized string to generate a new URL #trickyBoi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,9 @@
 @section('title', 'Logiciels de gestion de garderie')
 
 @section('content')
-    <div class="row mb-4">
-        <div class="col">
-            <div class="alert alert-primary alert-guided-tour">
-                Nouvel utilisateur ? Suivez la visite guidée pour découvrir en quoi <em>Garderies</em> peut vous simplifier la gestion de vos structures d'accueil.
-                <a href="{{config('app.url')}}/?{{str_random(5)}}#tour">Démarrer la visite</a>
-            </div>
-        </div>
+    <div class="alert alert-primary alert-guided-tour">
+        Nouvel utilisateur ? Suivez la visite guidée pour découvrir en quoi <em>Garderies</em> peut vous simplifier la gestion de vos structures d'accueil.
+        <a href="{{config('app.url')}}/?{{str_random(5)}}#tour">Démarrer la visite</a>
     </div>
 
     <div class="row mb-4 dashboard__summary">
