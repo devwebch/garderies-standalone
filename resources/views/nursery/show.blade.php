1121243d7f01145064a13c22a9bd814ea7fee4ba
Simon Rapin <simon.rapin@gmail.com>
2018-07-10 15:29:11 +0200

    -updated: network color is now displayed on lists -updated: vue table empty message -added: network filter on nursery vue table

--- resources/views/nursery/show.blade.php
+++ resources/views/nursery/show.blade.php
@@ -20,7 +20,7 @@
                     <p><strong>E-mail :</strong> {{$nursery->email}}</p>
                     <p>
                         <strong>Réseau :</strong>
-                        <span class="badge text-white" style="background-color: {{$nursery->network->color}};">{{$nursery->network->name ?? '-'}}</span>
+                        <span class="badge text-white" style="background-color: {{$nursery->network->color ?? '#ccc'}};">{{$nursery->network->name ?? '-'}}</span>
                     </p>
                 </div>
             </div>
