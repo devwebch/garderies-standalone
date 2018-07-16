138c1e491bc8335bf4fcf63ba5ba27c05ae83b9a
Simon Rapin <simon.rapin@gmail.com>
2018-07-16 11:53:33 +0200

    -updated: user factory now include random swiss phone number

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name'              => $faker->firstName('female') . ' ' . $faker->lastName,
         'email'             => $faker->unique()->safeEmail,
-        'phone'             => $faker->phoneNumber,
+        'phone'             => '+41 79 ' . rand(300, 500) . ' ' . rand(20, 90) . ' ' . rand(20, 90),
         'nursery_id'        => rand(1, 15),
         'diploma_id'        => rand(1, $diplomas),
         'password'          => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
