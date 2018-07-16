c4462e00a358916656e8c419d666ab47e9550601
Simon Rapin <simon.rapin@gmail.com>
2018-07-16 11:23:16 +0200

    -updated: user profile to handle empty lists

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -63,119 +63,132 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <table class="table table-borderless table-striped table-responsive-lg">
-                                <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Début</th>
-                                    <th>Fin</th>
-                                    <th width="30">Status</th>
-                                </tr>
-                                </thead>
-                                @foreach($availabilities as $availability)
+                            @if (!$availabilities->count())
+                                <div class="alert alert-info">Aucune disponibilité renseignée pour le moment.</div>
+                                <a href="{{route('users.availabilities', $user->id)}}" class="btn btn-info"><i class="fas fa-calendar"></i> Gérer mes disponibilités</a>
+                            @else
+                                <table class="table table-borderless table-striped table-responsive-lg">
+                                    <thead>
                                     <tr>
-                                        <td>{{$availability->start->format('d.m.Y')}}</td>
-                                        <td>{{$availability->start->format('H\hi')}}</td>
-                                        <td>{{$availability->end->format('H\hi')}}</td>
-                                        <td>
-                                            @switch($availability->status)
-                                                @case(\App\Availability::STATUS_UNTOUCHED)
-                                                <span class="badge badge-info">Libre</span>
-                                                @break
-                                                @case(\App\Availability::STATUS_BOOKED)
-                                                <span class="badge badge-success">Réservé</span>
-                                                @break
-                                                @case(\App\Availability::STATUS_ARCHIVED)
-                                                <span class="badge badge-dark">Archivé</span>
-                                                @break
-                                            @endswitch
-                                        </td>
-                                        <td>
-                                            @if ($availability->request)
-                                                <a href="{{route('booking-requests.show', $availability->request)}}">Voir</a>
-                                            @endif
-                                        </td>
+                                        <th>Date</th>
+                                        <th>Début</th>
+                                        <th>Fin</th>
+                                        <th width="30">Status</th>
                                     </tr>
-                                @endforeach
-                            </table>
+                                    </thead>
+                                    @foreach($availabilities as $availability)
+                                        <tr>
+                                            <td>{{$availability->start->format('d.m.Y')}}</td>
+                                            <td>{{$availability->start->format('H\hi')}}</td>
+                                            <td>{{$availability->end->format('H\hi')}}</td>
+                                            <td>
+                                                @switch($availability->status)
+                                                    @case(\App\Availability::STATUS_UNTOUCHED)
+                                                    <span class="badge badge-info">Libre</span>
+                                                    @break
+                                                    @case(\App\Availability::STATUS_BOOKED)
+                                                    <span class="badge badge-success">Réservé</span>
+                                                    @break
+                                                    @case(\App\Availability::STATUS_ARCHIVED)
+                                                    <span class="badge badge-dark">Archivé</span>
+                                                    @break
+                                                @endswitch
+                                            </td>
+                                            <td>
+                                                @if ($availability->request)
+                                                    <a href="{{route('booking-requests.show', $availability->request)}}">Voir</a>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            @endif
                         </div>
                     </div>
                     <div class="card card-default mb-4">
                         <div class="card-header">Demandes en attente</div>
                         <div class="card-body">
-                            <table class="table table-borderless table-striped table-responsive-lg">
-                                <thead>
-                                <tr>
-                                    <th>Jour</th>
-                                    <th>Début</th>
-                                    <th>Fin</th>
-                                    <th>Etablissement</th>
-                                    <th width="120">Status</th>
-                                    <th width="50"></th>
-                                </tr>
-                                </thead>
-                                @foreach($bookingRequests as $request)
+                            @if (!$bookingRequests->count())
+                                <div class="alert alert-info">Aucune demande en attente.</div>
+                            @else
+                                <table class="table table-borderless table-striped table-responsive-lg">
+                                    <thead>
                                     <tr>
-                                        <td>{{$request->start->format('d.m.Y')}}</td>
-                                        <td>{{$request->start->format('H\hi')}}</td>
-                                        <td>{{$request->end->format('H\hi')}}</td>
-                                        <td><a href="{{route('nurseries.show', $request->nursery ?? 0)}}">{{$request->nursery->name ?? '-'}}</a></td>
-                                        <td>
-                                            @switch($request->status)
-                                                @case(\App\Booking::STATUS_PENDING)
-                                                <span class="badge badge-info">En attente</span>
-                                                @break
-                                                @case(\App\Booking::STATUS_APPROVED)
-                                                <span class="badge badge-success">Validé</span>
-                                                @break
-                                                @case(\App\Booking::STATUS_ARCHIVED)
-                                                <span class="badge badge-dark">Archivé</span>
-                                                @break
-                                            @endswitch
-                                        </td>
-                                        <td><a href="{{route('booking-requests.show', $request)}}">Voir</a></td>
+                                        <th>Jour</th>
+                                        <th>Début</th>
+                                        <th>Fin</th>
+                                        <th>Etablissement</th>
+                                        <th width="120">Status</th>
+                                        <th width="50"></th>
                                     </tr>
-                                @endforeach
-                            </table>
+                                    </thead>
+                                    @foreach($bookingRequests as $request)
+                                        <tr>
+                                            <td>{{$request->start->format('d.m.Y')}}</td>
+                                            <td>{{$request->start->format('H\hi')}}</td>
+                                            <td>{{$request->end->format('H\hi')}}</td>
+                                            <td><a href="{{route('nurseries.show', $request->nursery ?? 0)}}">{{$request->nursery->name ?? '-'}}</a></td>
+                                            <td>
+                                                @switch($request->status)
+                                                    @case(\App\Booking::STATUS_PENDING)
+                                                    <span class="badge badge-info">En attente</span>
+                                                    @break
+                                                    @case(\App\Booking::STATUS_APPROVED)
+                                                    <span class="badge badge-success">Validé</span>
+                                                    @break
+                                                    @case(\App\Booking::STATUS_ARCHIVED)
+                                                    <span class="badge badge-dark">Archivé</span>
+                                                    @break
+                                                @endswitch
+                                            </td>
+                                            <td><a href="{{route('booking-requests.show', $request)}}">Voir</a></td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            @endif
                         </div>
                     </div>
                     <div class="card card-default">
                         <div class="card-header">Vos prochains remplacements</div>
                         <div class="card-body">
-                            <table class="table table-borderless table-striped table-responsive-lg">
-                                <thead>
-                                <tr>
-                                    <th>Jour</th>
-                                    <th>Début</th>
-                                    <th>Fin</th>
-                                    <th>Etablissement</th>
-                                    <th width="120">Status</th>
-                                    <th width="50"></th>
-                                </tr>
-                                </thead>
-                                @foreach($bookings as $booking)
+                            @if (!$bookings->count())
+                                <div class="alert alert-info">Aucun remplacement prévu.</div>
+                            @else
+                                <table class="table table-borderless table-striped table-responsive-lg">
+                                    <thead>
                                     <tr>
-                                        <td>{{$booking->start->format('d.m.Y')}}</td>
-                                        <td>{{$booking->start->format('H\hi')}}</td>
-                                        <td>{{$booking->end->format('H\hi')}}</td>
-                                        <td><a href="{{route('nurseries.show', $booking->nursery ?? 0)}}">{{$booking->nursery->name ?? '-'}}</a></td>
-                                        <td>
-                                            @switch($booking->status)
-                                                @case(\App\Booking::STATUS_PENDING)
-                                                <span class="badge badge-info">En attente</span>
-                                                @break
-                                                @case(\App\Booking::STATUS_APPROVED)
-                                                <span class="badge badge-success">Validé</span>
-                                                @break
-                                                @case(\App\Booking::STATUS_ARCHIVED)
-                                                <span class="badge badge-dark">Archivé</span>
-                                                @break
-                                            @endswitch
-                                        </td>
-                                        <td><a href="{{route('bookings.show', $booking)}}">Voir</a></td>
+                                        <th>Jour</th>
+                                        <th>Début</th>
+                                        <th>Fin</th>
+                                        <th>Etablissement</th>
+                                        <th width="120">Status</th>
+                                        <th width="50"></th>
                                     </tr>
-                                @endforeach
-                            </table>
+                                    </thead>
+                                    @foreach($bookings as $booking)
+                                        <tr>
+                                            <td>{{$booking->start->format('d.m.Y')}}</td>
+                                            <td>{{$booking->start->format('H\hi')}}</td>
+                                            <td>{{$booking->end->format('H\hi')}}</td>
+                                            <td><a href="{{route('nurseries.show', $booking->nursery ?? 0)}}">{{$booking->nursery->name ?? '-'}}</a></td>
+                                            <td>
+                                                @switch($booking->status)
+                                                    @case(\App\Booking::STATUS_PENDING)
+                                                    <span class="badge badge-info">En attente</span>
+                                                    @break
+                                                    @case(\App\Booking::STATUS_APPROVED)
+                                                    <span class="badge badge-success">Validé</span>
+                                                    @break
+                                                    @case(\App\Booking::STATUS_ARCHIVED)
+                                                    <span class="badge badge-dark">Archivé</span>
+                                                    @break
+                                                @endswitch
+                                            </td>
+                                            <td><a href="{{route('bookings.show', $booking)}}">Voir</a></td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            @endif
                         </div>
                     </div>
                 </div>
