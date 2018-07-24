78fa2ae12a4a24012aa2122f3f51e0fc900a7f17
Simon Rapin <simon.rapin@gmail.com>
2018-07-24 15:17:56 +0200

    -updated: now includes a wrapper to allow for vuejs instanciation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,18 +2,20 @@
 <html lang="{{ app()->getLocale() }}">
     @include('common.head')
     <body>
-        @include('common.header')
-        <div class="main" id="app">
-            @yield('hook-vue')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        @yield('content')
+        <div class="wrapper" id="app">
+            @include('common.header')
+            <div class="main">
+                @yield('hook-vue')
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            @yield('content')
+                        </div>
                     </div>
                 </div>
             </div>
+            @include('common.footer')
         </div>
-        @include('common.footer')
         @include('common.scripts')
     </body>
 </html>
--- resources/views/layouts/two-columns.blade.php
+++ resources/views/layouts/two-columns.blade.php
@@ -2,21 +2,23 @@
 <html lang="{{ app()->getLocale() }}">
     @include('common.head')
     <body>
-        @include('common.header')
-        <div class="main" id="app">
-            @yield('hook-vue')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-xl-2 mb-4 mb-lg-0 d-print-none">
-                        @yield('nav-lateral')
-                    </div>
-                    <div class="col-md-9 col-xl-10">
-                        @yield('content')
+        <div class="wrapper" id="app">
+            @include('common.header')
+            <div class="main">
+                @yield('hook-vue')
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-xl-2 mb-4 mb-lg-0 d-print-none">
+                            @yield('nav-lateral')
+                        </div>
+                        <div class="col-md-9 col-xl-10">
+                            @yield('content')
+                        </div>
                     </div>
                 </div>
             </div>
+            @include('common.footer')
         </div>
-        @include('common.footer')
         @include('common.scripts')
     </body>
 </html>
