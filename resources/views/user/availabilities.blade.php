6b68626d5fc7dfe70c52962a8d22175b6f281048
Simon Rapin <simon.rapin@gmail.com>
2018-07-25 13:34:33 +0200

    -updated: availabilities are now full width

--- resources/views/user/availabilities.blade.php
+++ resources/views/user/availabilities.blade.php
@@ -1,12 +1,19 @@
-@extends('layouts.two-columns')
+@extends('layouts.app')
 
 @section('title', 'Disponibilités')
 
 @section('content')
-    <div class="card card-default">
-        <div class="card-header">{{$user->name}}</div>
-        <div class="card-body">
-            <user-availabilities user="{{$user->id}}"></user-availabilities>
+    <div class="wrapper">
+        <div class="row">
+            <div class="col mb-2">
+                <a href="{{route('users.show', $user)}}" class="btn btn-info btn-sm">&larr; Retour au profil</a>
+            </div>
+        </div>
+        <div class="card card-default">
+            <div class="card-header">{{$user->name}}</div>
+            <div class="card-body">
+                <user-availabilities user="{{$user->id}}"></user-availabilities>
+            </div>
         </div>
     </div>
 @endsection
