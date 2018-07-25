ee662f34dd09fd07f491e76c2c08ce9558cbf7fd
Simon Rapin <simon.rapin@gmail.com>
2018-07-25 15:27:00 +0200

    -added: cachable models

--- config/cache.php
+++ config/cache.php
@@ -62,7 +62,7 @@ return [
             'servers' => [
                 [
                     'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 32769),
+                    'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
             ],
